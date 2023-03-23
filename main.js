b564f5bec13b9546c1eeebacaecc3b33457020a2
NPCDALAL <124927029+NPCDALAL@users.noreply.github.com>
2023-03-23 12:41:08 -0600

    s

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // COIN FLIP SIMULATOR
+
+// HTML Variables
+let outputE1 = document.getElementById("output");
+let headsEl = document.getElementById("heads-out");
+let tailsEl = document.getElementById("tails-out");
+// Count Variables
+let numHeads = 0;
+let numTails = 0;
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Generate a Random Number
+  let randNum = Math.random();
+  console.log(randNum);
+
+  // Simulate the Coin Flip
+  if (randNum < 0.5) {
+    outputE1.innerHTML = "<img src='img/heads.png' />";
+    numHeads++;
+    headsEl.innerHTML = numHeads;
+  } else {
+    outputE1.innerHTML = "<img src='img/tails.png' />";
+    numTails++;
+    tailsEl.innerHTML = numTails;
+  }
+}
